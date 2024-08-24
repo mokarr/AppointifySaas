d840854cb4f61319bf4e64567ea9d7fca682f826
mohamed-karroumi <mokarr246@gmail.com>
2024-08-24 17:31:21 +0200

    feat: database connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   schema: './src/models/Schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? '',
+    url:
+      process.env.DATABASE_URL ??
+      'postgresql://postgres.gdyoqutidqeeszjkvtxo:RZh*DiYd@KEV3km@aws-0-eu-central-1.pooler.supabase.com:6543/postgres',
   },
   verbose: true,
   strict: true,
