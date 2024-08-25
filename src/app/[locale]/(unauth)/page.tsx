4bff0af96538cfeabc30827053bdc8c5406e9122
mohamed-karroumi <mokarr246@gmail.com>
2024-08-25 15:09:09 +0200

    fix: test3

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,11 +1,8 @@
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 
-import { CTA } from '@/templates/CTA';
-import { FAQ } from '@/templates/FAQ';
 import { Features } from '@/templates/Features';
-import { Footer } from '@/templates/Footer';
 import { Hero } from '@/templates/Hero';
-import { Pricing } from '@/templates/Pricing';
+import { Navbar } from '@/templates/Navbar';
 import { Sponsors } from '@/templates/Sponsors';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
@@ -26,14 +23,14 @@ export default function IndexPage(props: { params: { locale: string } }) {
   return (
     <>
       <p>hello</p>
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
       <Sponsors />
       <Features />
-      <Pricing />
+      {/* <Pricing />
       <FAQ />
       <CTA />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
