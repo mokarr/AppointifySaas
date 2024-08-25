f749fe104ebf7430f04ab6ee580efd8a169304d5
mohamed-karroumi <mokarr246@gmail.com>
2024-08-25 14:55:15 +0200

    fix: localeSwitcher

--- src/templates/Navbar.tsx
+++ src/templates/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
-import LocaleSwitcher from '@/components/LocaleSwitcher';
 import { buttonVariants } from '@/components/ui/button';
 import { CenteredMenu } from '@/features/landing/CenteredMenu';
 import { Section } from '@/features/landing/Section';
@@ -17,9 +16,9 @@ const Navbar = () => {
         logo={<Logo />}
         rightMenu={
           <>
-            <li>
+            {/* <li>
               <LocaleSwitcher />
-            </li>
+            </li> */}
             <li>
               <Link href="/sign-in">{t('sign_in')}</Link>
             </li>
